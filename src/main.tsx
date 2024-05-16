caad9de81f7d4b56aae7645e7e616f0816bc3a3c
SAGUN <sagungurung614@gmail.com>
2024-05-16 14:04:11 +0545

    Toast message and form reset success

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./store/store.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
+      <ToastContainer></ToastContainer>
       <App />
     </Provider>
   </BrowserRouter>
