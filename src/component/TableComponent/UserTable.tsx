7535256a943471ffe3390ea0f5623650e7ade8af
SAGUN <sagungurung614@gmail.com>
2024-06-07 12:33:05 +0545

    Merge Merge

--- src/component/TableComponent/UserTable.tsx
+++ src/component/TableComponent/UserTable.tsx
@@ -50,7 +50,7 @@ const UserTable: React.FC = () => {
   }
 
   const handleEditClick = (selectedRowData: IData[]) => {
-    navigate(`/admin/forms/users/update/${selectedRowData[0]._id}`);
+    navigate(`/admin/forms/users/update/${selectedRowData[0].id}`);
   };
 
   const handleViewClick = (selectedRowData: IData[]) => {
