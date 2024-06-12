6fe571fdb3bacd1a51cb48b2a85e1db3be8c6916
SAGUN <sagungurung614@gmail.com>
2024-06-12 10:52:31 +0545

    Sanjog stuff

--- src/DeleteConfirmation.tsx
+++ src/DeleteConfirmation.tsx
@@ -1,12 +1,9 @@
-import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import DialogContent from "@mui/material/DialogContent";
 import Typography from "@mui/material/Typography";
+import * as React from "react";
 
 interface DeleteConfirmationProps {
   open: boolean;
@@ -25,27 +22,21 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({
       aria-labelledby="customized-dialog-title"
       open={open}
     >
-      {/* <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-        Delete
-        <IconButton
-          aria-label="close"
-          onClick={onCancel}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle> */}
       <DialogContent>
         <Typography gutterBottom>Are you sure you want to delete?</Typography>
       </DialogContent>
       <DialogActions sx={{ justifyContent: "center" }}>
-        <Button onClick={onCancel}>Cancel</Button>
-        <Button onClick={onConfirm} autoFocus>
+        <Button
+          sx={{ "&:hover": { background: "#FF7F7F", color: "white" } }}
+          onClick={onCancel}
+        >
+          Cancel
+        </Button>
+        <Button
+          sx={{ "&:hover": { background: "#90EE90", color: "white" } }}
+          onClick={onConfirm}
+          autoFocus
+        >
           Ok
         </Button>
       </DialogActions>
