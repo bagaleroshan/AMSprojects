cf864dd1589b0cb92f8cad3e24a7797481b3a110
SAGUN <sagungurung614@gmail.com>
2024-06-14 14:16:04 +0545

    Group Form On the way

--- src/component/group/UpdateGroup.tsx
+++ src/component/group/UpdateGroup.tsx
@@ -1,8 +1,7 @@
 import { toast } from "react-toastify";
 import GroupForm from "./GroupForm";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IGroup } from "../interfaces/GroupInterface";
-
 import { useNavigate, useParams } from "react-router-dom";
 import {
   getErrorMessage,
@@ -17,9 +16,10 @@ import {
 const UpdateGroup = () => {
   const params = useParams();
   const navigate = useNavigate();
+  const [initialGroup, setInitialGroup] = useState<IGroup | null>(null);
 
   const [
-    updateSubject,
+    updateGroup,
     {
       isError: isErrorUpdateGroup,
       isSuccess: isSuccessUpdateGroup,
@@ -34,45 +34,66 @@ const UpdateGroup = () => {
     error: errorViewSpecific,
   } = useReadGroupByIdQuery(params.id);
 
-  const Group = dataViewSpecific?.result || {};
+  // const group = dataViewSpecific?.result;
+  console.log(dataViewSpecific?.result);
 
   const submitValue = async (values: IGroup) => {
-    updateSubject({ id: params.id, body: values });
+    updateGroup({ id: params.id, body: values });
   };
+  /* Converitng string to date */
+  useEffect(() => {
+    if (dataViewSpecific?.result) {
+      const groupWithDate = {
+        ...dataViewSpecific.result,
+        // Assuming the date field in your group object is named 'dateField'
+        startTime: new Date(dataViewSpecific.result.startTime),
+        endTime: new Date(dataViewSpecific.result.endTime),
+      };
+      setInitialGroup(groupWithDate);
+    }
+  }, [dataViewSpecific]);
 
   useEffect(() => {
-    isErrorUpdateGroup &&
-      (isFetchBaseQueryError(errorUpdateGroup)
-        ? toast.error(getErrorMessage(errorUpdateGroup))
-        : isSerializedError(errorUpdateGroup)
-        ? toast.error(errorUpdateGroup?.message)
-        : "Unknown Error");
+    if (isErrorUpdateGroup) {
+      if (isFetchBaseQueryError(errorUpdateGroup)) {
+        toast.error(getErrorMessage(errorUpdateGroup));
+      } else if (isSerializedError(errorUpdateGroup)) {
+        toast.error(errorUpdateGroup.message);
+      } else {
+        toast.error("Unknown Error");
+      }
+    }
   }, [isErrorUpdateGroup, errorUpdateGroup]);
 
   useEffect(() => {
     if (isSuccessUpdateGroup) {
       toast.success("Group Updated Successfully");
-      navigate("/admin/Groups");
+      navigate("/admin/groups");
     }
-  });
+  }, [isSuccessUpdateGroup, navigate]);
 
   useEffect(() => {
-    isErrorViewSpecific &&
-      (isFetchBaseQueryError(errorViewSpecific)
-        ? toast.error(getErrorMessage(errorViewSpecific))
-        : isSerializedError(errorViewSpecific)
-        ? toast.error(errorViewSpecific?.message)
-        : "Unknown Error");
+    if (isErrorViewSpecific) {
+      if (isFetchBaseQueryError(errorViewSpecific)) {
+        toast.error(getErrorMessage(errorViewSpecific));
+      } else if (isSerializedError(errorViewSpecific)) {
+        toast.error(errorViewSpecific.message);
+      } else {
+        toast.error("Unknown Error");
+      }
+    }
   }, [isErrorViewSpecific, errorViewSpecific]);
 
   return (
     <div>
-      <GroupForm
-        buttonName="Update Group"
-        isLoading={isLoadingUpdateGroup}
-        onSubmit={submitValue}
-        group={Group}
-      />
+      {initialGroup && (
+        <GroupForm
+          buttonName="Update Group"
+          isLoading={isLoadingUpdateGroup}
+          onSubmit={submitValue}
+          group={initialGroup}
+        />
+      )}
     </div>
   );
 };
