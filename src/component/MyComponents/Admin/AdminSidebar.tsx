286e7ba27ac160a9daa6d9b7dc86ed9acd8dd12c
Dikshya  Bhandari <dikshyabhandari01@gmail.com>
2024-06-25 11:13:08 +0545

    updated

--- src/component/MyComponents/Admin/AdminSidebar.tsx
+++ src/component/MyComponents/Admin/AdminSidebar.tsx
@@ -17,6 +17,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { ClickAwayListener } from "@mui/base/ClickAwayListener";
+import MenuBookOutlinedIcon from "@mui/icons-material/MenuBookOutlined";
 
 import Toolbar from "@mui/material/Toolbar";
 import { useTheme } from "@mui/material/styles";
@@ -315,6 +316,59 @@ export default function AdminSidebar() {
                       }}
                     >
                       <EmailIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary="Message"
+                      sx={{
+                        opacity: open ? 1 : 0,
+                        "&:hover": { color: "#0195CF" },
+                      }}
+                    />
+                  </ListItemButton>
+                </LightTooltip>
+              </ListItem>
+              {/* ********Classes************ */}
+              <ListItem
+                disablePadding
+                sx={{
+                  display: "block",
+                  "&:hover": {
+                    color: "#0195CF",
+                  },
+                  backgroundColor:
+                    location.pathname === "/admin/class"
+                      ? "rgba(239,249,255,1)"
+                      : "default",
+                  color:
+                    location.pathname === "/admin/messages"
+                      ? "#0195CF"
+                      : "default",
+                }}
+                onClick={() => handleNavigate("/admin/messages")}
+              >
+                <LightTooltip title="Message" placement="right">
+                  <ListItemButton
+                    sx={{
+                      minHeight: 48,
+                      justifyContent: open ? "initial" : "center",
+                      px: 2.5,
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : "auto",
+                        justifyContent: "center",
+                        "&:hover": {
+                          color: "#0195CF",
+                        },
+                        color:
+                          location.pathname === "/admin/messages"
+                            ? "#0195CF"
+                            : "default",
+                      }}
+                    >
+                      <MenuBookOutlinedIcon />
                     </ListItemIcon>
                     <ListItemText
                       primary="Message"
