0fbe50846fdfaf83d47256c00fb2739639d62fe8
Jen Hona <jenishona123@gmail.com>
2024-07-24 13:05:24 +0545

    Jenis Final

--- src/component/theme/MuiLoadingButtonTheme.tsx
+++ src/component/theme/MuiLoadingButtonTheme.tsx
@@ -1,15 +1,12 @@
+import React from "react";
 import SendIcon from "@mui/icons-material/Send";
 import { LoadingButton } from "@mui/lab";
-import { Button, ThemeProvider, createTheme } from "@mui/material";
-import React from "react";
+import { Button, ThemeProvider, colors, createTheme } from "@mui/material";
 
 const theme = createTheme({
   components: {
     MuiLoadingButton: {
       styleOverrides: {
-        // circularProgress: {
-        //   color: "#ffffff", // Bright white color for the loading indicator
-        // },
         root: {
           "& .MuiCircularProgress-root": {
             color: "#ffffff", // Bright white color for the CircularProgress
@@ -41,19 +38,21 @@ const MuiLoadingButtonTheme: React.FC<{
           type={type}
           sx={{
             backgroundColor: "primary.main",
-            color: "white",
             mt: 3,
             mb: 2,
-            "& .MuiButton-label": {
-              color: "white", // Text color when not loading
+            '& .MuiLoadingButton-loadingIndicator': {
+              color: '#ffffff', // Bright white color for the CircularProgress
             },
-            "& .MuiCircularProgress-root": {
-              color: "white", // Bright white color for the CircularProgress
+            '& .MuiButton-root': {
+              color: 'white', // Ensure the button text is white
+            },
+            '& .MuiLoadingButton-root': {
+              color: 'white', // Ensure the button text is white
             },
           }}
           fullWidth
         >
-          LOADING .....
+          <p style={{color:'white'}}>Loading ....</p>
         </LoadingButton>
       ) : (
         <Button
