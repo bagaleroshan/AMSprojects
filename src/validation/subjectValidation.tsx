f423f1c3fa5ed1321a752e3b235a74416617ea33
SAGUN <sagungurung614@gmail.com>
2024-07-24 11:09:03 +0545

    Subject Validation

--- src/validation/subjectValidation.tsx
+++ src/validation/subjectValidation.tsx
@@ -6,8 +6,9 @@ export const validationSchema = yup.object({
     .min(3)
     .max(100)
     .required("Subject name is required"),
-  subjectCode: yup.string().min(3).max(30).required("Subject Code is required"),
-  numberOfClasses: yup
-    .number()
-    .required("Please choose the number of classes"),
+  subjectCode: yup
+    .string()
+    // .min(3).max(30)
+    .required("Subject Code is required"),
+  numberOfClasses: yup.number().required("Please choose the number of classes"),
 });
